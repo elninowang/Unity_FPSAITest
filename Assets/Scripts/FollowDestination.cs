1ca7a866c992ff254a3db417eeb9725a6432caa9
elninowang <elninowang@qq.com>
2017-10-11 22:19:38 +0800

    first submit

---
+++ Assets/Scripts/FollowDestination.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowDestination : MonoBehaviour {
+
+    public NavMeshAgent ThisAgent = null;
+    public Transform Destination = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        ThisAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ThisAgent.SetDestination(Destination.position);
+	}
+}
